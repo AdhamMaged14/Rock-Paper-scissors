113860b8879e2cdc39f2f44d150b7e1005d98abc
AdhamMaged14 <adhammaged179@gmail.com>
2025-07-17 15:36:02 +0300

    Implemented getComputerChoice function

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+function getComputerChoice() {
+    randomNumber= Math.random();
+    if (randomNumber < 1/3) {
+        return "rock";
+    } else if (randomNumber < 2/3) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+  
+}
